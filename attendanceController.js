d6d7b5bd13df5798caf9a0c6d07db1b86050c6a7
Benjamin Corn <bcorn@bu.edu>
2016-09-08 11:33:22 -0400

    main

---
+++ attendanceController.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    "use strict";
+
+    // Retrieving inventory module
+    angular.module("attendance")
+        .controller("attendanceController", attendanceController);
+
+    // code for controller itself
+    function attendanceController($http) {
+
+        var vm = this;
+        vm.Message = "";
+
+        vm.CheckIn = function () {
+            var payload = {Section: vm.Section, BUId: vm.BUId, SecretCode: vm.SecretCode };
+
+            $http.post("https://www.inviodev.com/api/attendance/checkin", payload)
+            .then(function successCallback(response) {
+                vm.Message = response.data;
+            }, function errorCallback(response) {
+                vm.Message = response.data;
+            });
+        };
+        
+
+    }
+
+})();
